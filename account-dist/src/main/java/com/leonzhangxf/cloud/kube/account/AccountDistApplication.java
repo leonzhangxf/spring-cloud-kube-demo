82c80eb6cacd5c5fa3150efa362af57f05f3258e
leonzhangxf <leon_zhangxf@qq.com>
2019-07-19 16:02:30 +0800

    = 调整配置 + 添加启动信息

--- account-dist/src/main/java/com/leonzhangxf/cloud/kube/account/AccountDistApplication.java
+++ account-dist/src/main/java/com/leonzhangxf/cloud/kube/account/AccountDistApplication.java
@@ -1,20 +1,52 @@
 package com.leonzhangxf.cloud.kube.account;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.util.CollectionUtils;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.List;
 
 /**
  * @author leonzhangxf
  * @date 20190718
  */
+@Slf4j
 @SpringCloudApplication
 @EnableScheduling
 @EnableSwagger2WebMvc
-public class AccountDistApplication {
+public class AccountDistApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(AccountDistApplication.class).run(args);
+    }
+
+    private DiscoveryClient discoveryClient;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("services {}", discoveryClient.getServices());
+        List<ServiceInstance> gatewayInstances = discoveryClient.getInstances("gateway-dist");
+        if (!CollectionUtils.isEmpty(gatewayInstances)) {
+            ServiceInstance serviceInstance = gatewayInstances.get(0);
+            log.info("==== {}", serviceInstance.getUri().toString());
+            log.info("==== {}", serviceInstance.getServiceId());
+            log.info("==== {}", serviceInstance.getInstanceId());
+            log.info("==== {}", serviceInstance.getScheme());
+            log.info("==== {}", serviceInstance.getHost());
+            log.info("==== {}", serviceInstance.getPort());
+            log.info("==== {}", serviceInstance.getMetadata());
+        }
+    }
+
+    @Autowired
+    public void setDiscoveryClient(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
     }
 }
--- gateway-dist/src/main/java/com/leonzhangxf/cloud/kube/gateway/GatewayDistApplication.java
+++ gateway-dist/src/main/java/com/leonzhangxf/cloud/kube/gateway/GatewayDistApplication.java
@@ -2,13 +2,20 @@ package com.leonzhangxf.cloud.kube.gateway;
 
 import com.leonzhangxf.cloud.kube.account.api.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
+
+import java.util.List;
 
 /**
  * @author leonzhangxf
@@ -20,7 +27,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 @EnableScheduling
 @EnableSwagger2WebFlux
 @Controller
-public class GatewayDistApplication {
+public class GatewayDistApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(GatewayDistApplication.class).run(args);
@@ -29,5 +36,28 @@ public class GatewayDistApplication {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    private DiscoveryClient discoveryClient;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("services {}", discoveryClient.getServices());
+        List<ServiceInstance> instances = discoveryClient.getInstances("account-dist");
+        if (!CollectionUtils.isEmpty(instances)) {
+            ServiceInstance serviceInstance = instances.get(0);
+            log.info("==== {}", serviceInstance.getUri().toString());
+            log.info("==== {}", serviceInstance.getServiceId());
+            log.info("==== {}", serviceInstance.getInstanceId());
+            log.info("==== {}", serviceInstance.getScheme());
+            log.info("==== {}", serviceInstance.getHost());
+            log.info("==== {}", serviceInstance.getPort());
+            log.info("==== {}", serviceInstance.getMetadata());
+        }
+    }
+
+    @Autowired
+    public void setDiscoveryClient(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
     }
 }
